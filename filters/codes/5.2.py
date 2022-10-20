1a33affa99102d348d17dbdf2265350b77eddea8
harshini <harshini.iith@gmail.com>
2022-10-20 22:36:33 +0530

    Pingala

--- filters/codes/5.2.py
+++ filters/codes/5.2.py
@@ -1,3 +1,151 @@
+import socket
+
+#WRITE CODE HERE:
+#1. Create a KEY-VALUE pairs (Create a dictionary OR Maintain a text file for KEY-VALUES).
+
+
+dst_ip = str(input("Enter Server IP: "))
+
+s = socket.socket()
+print ("Socket successfully created")
+
+dport = 12346
+
+s.bind((dst_ip, dport))
+print ("socket binded to %s" %(dport))
+
+s.listen(5)
+print ("socket is listening")
+
+
+
+
+
+
+
+
+
+cachedic = {}
+
+
+while True:
+		c, addr = s.accept()
+		print ('Got connection from', addr )
+		cacherecvmsg = c.recv(1024).decode()
+		#print('Server received '+recvmsg)
+		#c.send('Hello client'.encode())
+		
+	 
+		#Write your code here
+		#1. Uncomment c.send 
+		#2. Parse the received HTTP request
+		#3. Do the necessary operation depending upon whether it is GET, PUT or DELETE
+		#4. Send response
+		##################
+		
+		#recvmsg = c.recv(1024).decode()
+		
+		clientrequest = cacherecvmsg.split()
+
+		if(clientrequest[0]=="GET"):
+			    key = clientrequest[1].split("=")
+
+	        if key[len(key)-1] in cachedic.keys():
+				res = 'HTTP/1.1 200 OK ' + serverdic[key[len(key)-1]] + '\r\n\r\n'
+				c.send(res.encode())
+			else:
+                serverIP = "10.0.1.3"
+                dst_ip = str(input("Enter dstIP: "))
+                s = socket.socket()
+                print(dst_ip)
+                port = 12346
+                s.connect((dst_ip, port))
+
+                
+                s.send(cacherecvmsg.encode())
+                response = s.recv(1024).decode()
+                # print(response)
+                c.send(response.encode())
+			
+	
+		elif(clientrequest[0]=="PUT"):
+			        serverIP = "10.0.1.3"
+                    dst_ip = str(input("Enter dstIP: "))
+                    s = socket.socket()
+                    print(dst_ip)
+                    port = 12346
+                    s.connect((dst_ip, port))
+
+
+                    s.send(cacherecvmsg.encode())
+                    response = s.recv(1024).decode()
+                    #print(response)
+                    c.send(response.encode())
+			
+		elif(clientrequest[0]=="DELETE"):
+				key = clientrequest[1].split("/")
+				
+
+				if key[len(key)-1] in serverdic.keys():
+					 del serverdic[key[len(key)-1]]
+					 #res = 'HTTP/1.1 200 ok ' + 'deleated the key' + '\r\n\r\n' 
+					 #c.send(res.encode())
+
+
+					serverIP = "10.0.1.3"
+                    dst_ip = str(input("Enter dstIP: "))
+                    s = socket.socket()
+                    print(dst_ip)
+                    port = 12346
+                    s.connect((dst_ip, port))
+
+
+                    s.send(cacherecvmsg.encode())
+                    response = s.recv(1024).decode()
+                    # print(response)
+                    c.send(response.encode())
+			
+
+				else:
+
+					serverIP = "10.0.1.3"
+                    dst_ip = str(input("Enter dstIP: "))
+                    s = socket.socket()
+                    print(dst_ip)
+                    port = 12346
+                    s.connect((dst_ip, port))
+
+
+                    s.send(cacherecvmsg.encode())
+                    response = s.recv(1024).decode()
+                    #print(response)
+			        c.send(response.encode())
+
+
+
+
+
+
+
+
+c.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 import numpy as np
